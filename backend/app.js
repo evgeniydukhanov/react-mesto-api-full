326dab78faf9437266d642879f5c65aeda866bfe
evgeniydukhanov <duhanoves@itwis.org>
2022-05-10 17:51:15 +0700

    500err still here

--- backend/app.js
+++ backend/app.js
@@ -27,38 +27,34 @@ const errorCatcher = require('./errors/errorCatcher');
 const NotFoundError = require('./errors/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// const allowedCors = [
-//   'https://praktikum.tk',
-//   'http://praktikum.tk',
-//   'localhost:3000',
-//   'https://localhost:3000',
-//   'https://127.0.0.1:3000',
-//   'https://mesto-backend.nomoredomains.work',
-//   'https://localhost:3000',
-//   'https://127.0.0.1:3000',
-//   'http://mesto-backend.nomoredomains.work',
-// ];
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'https://localhost:3000',
+  'https://127.0.0.1:3000',
+  'https://mesto-backend.nomoredomains.work',
+  'https://localhost:3000',
+  'https://127.0.0.1:3000',
+  'http://mesto-backend.nomoredomains.work',
+];
 
 // eslint-disable-next-line prefer-arrow-callback
-// app.use(function (req, res, next) {
-//   const { origin } = req.headers;
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
-//   const { method } = req;
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   return next();
-// });
-
-app.use((req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
 });
 
 app.use(requestLogger);
